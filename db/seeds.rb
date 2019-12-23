9022677bc559e9e8e99c89127d2911373c02b98a
Etienne <etienne.burdet@gmail.com>
2019-12-23 22:15:03 +0100

    basic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Training.destroy_all
+Plan.destroy_all
+
+marathon = Plan.new
+marathon.name = 'marathon'
+marathon.start_date = Date.today()
+marathon.end_date = Date.today() + 120
+
+trail = Plan.new
+trail.name = 'trail'
+trail.start_date = Date.today()
+trail.end_date = Date.today() + 120
+
+(0...120).each_with_index do |_, index|
+  plan = marathon
+  date = Date.today + index
+  description = "3x5' au seuil parcours jazy"
+  Training.create(plan: plan, date: date, description: description)
+end
+
+(0...120).each_with_index do |_, index|
+  plan =trail
+  date = Date.today + index
+  description = "3x5' au seuil parcours jazy"
+  Training.create(plan: plan, date: date, description: description)
+end
